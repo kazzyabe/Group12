a907068f81837ccaa1b9afe1bf33b666cdd2691a
kazuki <kazuki@KY-MacBook-Air.local>
2016-06-13 16:57:31 -0700

    add welcome page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,7 @@ class UsersController < ApplicationController
       redirect_to '/signup'
     end
   end
-
+  
   private
   def user_params
     params.require(:user).permit(:username, :email, :password)
