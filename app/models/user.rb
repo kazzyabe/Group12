76ce0d8407a23c1734ec6646a515d140810a42f1
kazuki <kazuki@KY-MacBook-Air.local>
2016-06-29 00:01:54 -0700

    added setting on env/dev.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,10 +3,5 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-  has_secure_password
-  validates :username, :email, uniqueness: true, presence: true
-
-  def admin?
-    self.role == 'admin'
-  end
+         
 end
