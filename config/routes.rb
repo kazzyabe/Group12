877b64ce5b7ba50287840deb6d1112821a5d5638
kazuki <kazuki@KY-MacBook-Air.local>
2016-06-29 00:35:23 -0700

    fixed the links for devise

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root 'welcome#landing'
+  root to: 'welcome#landing'
   # You can have the root of your site routed with "root"
 
   #resources :sessions
