e24d9465a36d1a237fa4e8cc607db0746c435795
kazuki <kazuki@KY-MacBook-Air.local>
2016-06-29 01:02:28 -0700

    fixing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  
+
   root to: 'welcome#landing'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
