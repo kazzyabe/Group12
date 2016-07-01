8d578556302903bafdefdfffa23b60202fb173b2
Kazuki <yabekazyu@gmail.com>
2016-07-01 16:09:11 -0700

    modified to login with username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,20 @@ class User < ActiveRecord::Base
   :uniqueness => {
     :case_sensitive => false
   }
-  
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable,
+         :authentication_keys => [:username]
 
+ def self.find_first_by_auth_conditions(warden_conditions)
+   conditions = warden_conditions.dup
+   if login = conditions.delete(:login)
+     #認証の条件式を変更する
+     where(conditions).where(["username = :value", { :value => username }]).first
+   else
+     where(conditions).first
+   end
+ end
 end
