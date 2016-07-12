30a313a73e90ad418a4cf2dcc2b689c68e3008bb
James Araujo <araujojpm@gmail.com>
2016-07-11 21:03:50 -0700

    Styling done, error-checking done, app is ready for big feature implementation

--- db/schema.rb
+++ db/schema.rb
@@ -19,6 +19,10 @@ ActiveRecord::Schema.define(version: 20160629071958) do
   create_table "users", force: :cascade do |t|
     t.string   "username"
     t.string   "gender"
+    t.string   "interestedin"
+    t.integer  "age"
+    t.string   "biography"
+    t.string   "realname"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "role"
