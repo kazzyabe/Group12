20bc911d553134fcdc0de21db495b5b479a621fd
James Araujo <araujojpm@gmail.com>
2016-07-15 01:12:45 -0700

    Changes2

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,4 +1,22 @@
 class QuizzesController < ApplicationController
 
 
+def create
+  debugger
+@quiz = Quiz.new(quiz_params)
+if @quiz.save
+flash[:success] = "Updated"
+redirect_to @quiz
+else
+render 'new'
+end
+end
+
+  def quiz_params
+     params.require(:quiz).permit(:q1)
+   end
+
+   def show
+   end
+
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160715052707) do
+ActiveRecord::Schema.define(version: 20160715075119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,10 @@ ActiveRecord::Schema.define(version: 20160715052707) do
     t.string   "q5"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
+
+  add_index "quizzes", ["user_id"], name: "index_quizzes_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -51,4 +54,5 @@ ActiveRecord::Schema.define(version: 20160715052707) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "quizzes", "users"
 end
