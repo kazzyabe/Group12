3e6b8b56d1b2db4a6da08cfdf43f232b4ebebaaf
James Araujo <araujojpm@gmail.com>
2016-07-15 02:08:00 -0700

    Quiz creation implemented

--- app/controllers/quizzes_controller.rb
+++ app/controllers/quizzes_controller.rb
@@ -1,18 +1,21 @@
 class QuizzesController < ApplicationController
 
+  def edit
+    @quiz = Quiz.find(params:[id])
+  end
 
   def create
- @quiz = current_user.quiz.build(quiz_params)
+ @quiz = current_user.build_quiz(quiz_params)
     if @quiz.save
       flash[:success] = "Updated"
-      redirect_to @quiz
+      redirect_to '/'
     else
       render 'new'
     end
   end
 
   def quiz_params
-     params.require(:quiz).permit(:q1)
+     params.require(:quiz).permit(:q1, :q2, :q3, :q4, :q5)
    end
 
    def show
