a79e160f86a5db753fb48a09655c9deca881bfb9
James Araujo <araujojpm@gmail.com>
2016-07-19 20:25:46 -0700

    Quiz information updated when clicking button

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,14 @@ class WelcomeController < ApplicationController
   end
 
   def landing
+    if current_user != nil
+    if current_user.quiz.nil?
     @quiz = Quiz.new
+  else
+    @quiz = current_user.quiz(params[:id])
   end
-  
+else
+end
+  end
+
 end
