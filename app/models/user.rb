8dfcabeeec1bc158a194be6fc99cd7f42047c1ac
Justin Mang <jmang@sfu.ca>
2016-07-30 14:43:25 -0700

    added lat and long to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
 
+
+  geocoded_by :ip_address,
+    :latitude => :lat, :longitude => :lon
+  after_validation :geocode
+  
   has_one :quiz
 
   validates :username,
