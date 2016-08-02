0c3e0b0c97079448d3c95ff1dda620a08b587a63
Kazuki <yabekazyu@gmail.com>
2016-08-02 01:45:55 -0700

    account set up not working

---
+++ spec/features/account_setup_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.feature "admin management", :type => :feature do
+  background do
+    User.create!(username: 'admin', email: 'admin@sfu.ca', password: 'adminadmin', password_confirmation: 'adminadmin', role: 'admin')
+    visit new_user_session_path
+    fill_in 'username', with: 'admin'
+    fill_in 'password', with: 'adminadmin'
+    click_button 'Log in'
+  end
+
+  scenario "see Set up my account" do
+    expect(page).to have_link("Set up my Account")
+  end
+end
---
+++ spec/features/quizzes_feature_spec.rb
@@ -0,0 +1,15 @@
+require "rails_helper"
+
+RSpec.feature "admin management", :type => :feature do
+  background do
+    User.create!(username: 'admin', email: 'admin@sfu.ca', password: 'adminadmin', password_confirmation: 'adminadmin', role: 'admin')
+    visit new_user_session_path
+    fill_in 'username', with: 'admin'
+    fill_in 'password', with: 'adminadmin'
+    click_button 'Log in'
+  end
+
+  scenario "see Create/Edit Quiz" do
+    expect(page).to have_text("Create/Edit Quiz")
+  end
+end
