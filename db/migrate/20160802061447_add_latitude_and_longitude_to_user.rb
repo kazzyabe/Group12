ab9371988ef1a4c30540a1476b3f78f578ca9686
James Araujo <araujojpm@gmail.com>
2016-08-01 23:18:18 -0700

    Changed display of users for answers

--- db/migrate/20160802061447_add_latitude_and_longitude_to_user.rb
+++
@@ -1,8 +0,0 @@
-class AddLatitudeAndLongitudeToUser < ActiveRecord::Migration
-  def change
-    add_column :users, :latitude, :string
-    add_column :users, :float, :string
-    add_column :users, :longitude, :string
-    add_column :users, :float, :string
-  end
-end
